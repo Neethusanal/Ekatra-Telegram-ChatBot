cb0012f363912f90588cc3e31e5cae8b5e1ffcfd
Neethusanal <neethukumarkk@gmail.com>
2023-11-20 11:49:01 +0530

    "updates"

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+
 const TOKEN = process.env.TOKEN;
 
 const bot = new TelegramBot(TOKEN, { polling: false }, { filepath: false });
